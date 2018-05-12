543e7cd37872b8eff3b9aec4d8b6fbb70b197baf
Valeria Sofia Fuentes Rivera Silva <valeriasofiafuentesriverasilva@MacBook-Air-de-Valeria.local>
2018-05-12 19:21:39 +0200

    urls acabadas basandome en mi reset

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,25 @@
+"""
+https://docs.djangoproject.com/en/1.8/topics/http/urls/
+# Examples:
+# url(r'^$', 'myproject.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+"""
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from museos import views
+from django.contrib.auth.views import login, logout
+
+urlpatterns = [
+	url(r'^/?$',views.barra, name='pagina principal'),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^css/style.css$',views.css, name='css'),
+    url(r'^login/?$',views.userlogin, name='Usuario Login'),
+    url(r'^logout/?$',views.userlogout, name='Usuario Logout'),
+    url(r'^museos/?$',views.museos, name='Pag con todoslos museos'),
+    url(r'^museos/(.*)/?$',views.museo, name='Pag del museo'),
+    url(r'^(.*)/xml/?$',views.xml, name='canal xml'),
+    url(r'^about/?$',views.about, name='info en html autoria'),
+	url(r'^rss/?$',views.rss, name='comentarios'),
+	url(r'(.*)',views.usuario, name='Pagina del usuario'),
+]
